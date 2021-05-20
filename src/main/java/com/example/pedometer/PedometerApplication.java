9cd0e503e48d5aefd76fe98d20e3884277451122
Oscar Forss <oscar.forss.1@gmail.com>
2021-05-20 11:16:30 +0200

    First commit

---
+++ src/main/java/com/example/pedometer/PedometerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pedometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedometerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedometerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pedometer/PedometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pedometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedometerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
