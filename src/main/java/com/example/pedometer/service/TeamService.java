a325e2bca45ea5e137a638e195489014a85393c3
PatrikJMelander <patrikjmelander@gmail.com>
2021-05-25 15:42:10 +0200

    you can now only jion the same team ones

--- src/main/java/com/example/pedometer/service/TeamService.java
+++ src/main/java/com/example/pedometer/service/TeamService.java
@@ -39,11 +39,17 @@ public class TeamService {
                 .orElseThrow(() -> {
                     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "no team found");
                 });
+
         appUserRepository.findByEmail(userEmail)
                         .ifPresent(appUser -> {
+                            if (currentTeam.getTeamMembers().contains(appUser)){
+                                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You are already a member");
+                            }
                             currentTeam.getTeamMembers().add(appUser);
                             teamRepository.save(currentTeam);
                         });
+
+
 
         return currentTeam
                 .toResponse();
