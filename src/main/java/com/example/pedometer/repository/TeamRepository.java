50dc658b59cd6e2e198990e63740426a52b7b70f
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2021-05-27 09:10:06 +0200

    Add exists by team name in Repo

--- src/main/java/com/example/pedometer/repository/TeamRepository.java
+++ src/main/java/com/example/pedometer/repository/TeamRepository.java
@@ -4,6 +4,7 @@ import com.example.pedometer.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -17,4 +18,6 @@ import java.util.UUID;
 @Repository
 public interface TeamRepository extends JpaRepository<Team, UUID> {
     Optional<Team> findByTeamName(String teamName);
+    boolean existsByTeamName(String teamName);
+
 }
